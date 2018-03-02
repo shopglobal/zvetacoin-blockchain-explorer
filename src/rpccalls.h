81325084e768ee71b0aaf5eed31e86e0f39d5507
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-01 23:35:23 -0500

    Update rpccalls.h

--- src/rpccalls.h
+++ src/rpccalls.h
@@ -80,7 +80,7 @@ class rpccalls
 
 public:
 
-    rpccalls(string _deamon_url = "http:://127.0.0.1:18191",
+    rpccalls(string _deamon_url = "http:://127.0.0.1:24091",
              uint64_t _timeout = 200000);
 
     bool
