2c1acb7d1a807e423719652d0754cb0fff3f4a94
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-01 23:33:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -149,9 +149,9 @@ main(int ac, const char* av[])
 
     string deamon_url {*deamon_url_opt};
 
-    if (testnet && deamon_url == "http:://127.0.0.1:26968")
+    if (testnet && deamon_url == "http:://127.0.0.1:24091")
     {
-        deamon_url = "http:://127.0.0.1:36968";
+        deamon_url = "http:://127.0.0.1:28081";
     }
 
     uint64_t mempool_info_timeout {5000};
